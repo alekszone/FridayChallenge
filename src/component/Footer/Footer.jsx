97df80536695cff7379c518c397ebf0bb593900e
alekszone <50597224+alekszone@users.noreply.github.com>
2021-03-11 22:33:43 +0100

    Update Footer.jsx

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -3,14 +3,22 @@ import { Col } from "react-bootstrap";
 import { connect } from "react-redux";
 
 const mapStateToProps = (state) => state;
+const mapDispatchToProps = (dispatch, props) => ({
+  singelSong: (playSong) =>
+    dispatch({
+      type: "playSong",
+      payload: playSong,
+    }),
+});
 class Footer extends Component {
   state = {
     mute: false,
     currentSongTime: 0,
     duration: 0,
     volumState: 1,
+    playButton: false,
   };
-  volume = React.createRef("audio_tag");
+  volume = React.createRef();
 
   muteVolume = () => {
     if (!this.state.mute) {
@@ -34,105 +42,139 @@ class Footer extends Component {
   toggleMusic = () => {
     if (this.volume.current.paused) {
       this.volume.current.play();
+      this.setState({ playButton: true });
     } else {
       this.volume.current.pause();
+      this.setState({ playButton: false });
     }
   };
+  time = (time) => {
+    return (time - (time %= 60)) / 60 + (9 < time ? ":" : ":0") + ~~time;
+  };
+  songDuration = (sek) => {
+    let count = (sek.target.value * this.state.duration) / 100;
+    this.setState({ currentSongTime: count });
+    this.volume.current.currentTime = count;
+  };
+  playPrevious = () => {
+    if (this.props.allSongs.data !== 0) {
+      const findSong = this.props.allSongs.data.findIndex(
+        (x) => x.id === this.props.playSong.id
+      );
+      if (findSong && findSong.length !== 0) {
+        const selectPreviuSong = this.props.allSongs.data.slice(
+          findSong - 1,
+          findSong
+        );
+        if (selectPreviuSong) {
+          this.props.singelSong(selectPreviuSong[0]);
+        }
+        console.log(selectPreviuSong, "ca ka mrena");
+      } else {
+        this.props.singelSong(this.props.allSongs.data[0]);
+      }
+      console.log(findSong, "ca ka find");
+      console.log(this.props.allSongs.data[0], "ca ka id");
+    }
+  };
+  playNext = () => {};
 
   render() {
-    console.log(this.volume, "ca vlere merr volum");
+    console.log(
+      this.props.allSongs && this.props.allSongs.data,
+      "ca vlere ka allSongs"
+    );
     return (
       <div className="navbar fixed-bottom navbar-expand-sm w-100 d-flex justify-content-center ">
         <div className="row row-cols-1 row-cols-md-1 row-cols-lg-3 w-100 py-1">
           <Col></Col>
           <Col className=" d-flex flex-column justify-content-center align-items-center">
             <div className="d-flex  justify-content-center align-items-center mb-1">
-              <a href="#" className="mr-4">
+              <a className="mr-4">
                 <i className="fa fa-random"></i>
               </a>
-              <a href="#" className="mr-4">
+              <a className="mr-4" onClick={() => this.playPrevious()}>
                 <i className="fa fa-fast-backward"></i>
               </a>
               <a className="mr-4" onClick={() => this.toggleMusic()}>
-                <i
-                  style={{ fontSize: "2rem" }}
-                  className="fa fa-play-circle"
-                ></i>
+                {this.volume.current && this.volume.current.paused ? (
+                  <i
+                    style={{ fontSize: "2rem" }}
+                    className="fa fa-play-circle"
+                  ></i>
+                ) : (
+                  <i
+                    style={{ fontSize: "2rem" }}
+                    className="fa fa-pause-circle"
+                  ></i>
+                )}
               </a>
-              <a href="#" className="mr-4">
+              <a className="mr-4">
                 <i className="fa fa-fast-forward"></i>
               </a>
-              <a href="#" className="mr-4">
+              <a className="mr-1">
                 <i className="fa fa-repeat"></i>
               </a>
             </div>
-            <div className="w-100 d-flex align-items-baseline">
-              <small className="mr-2">0.00</small>
-              <div className="progress w-100 mb-2" style={{ height: "0.3rem" }}>
-                <div
-                  className="progress-bar w-75"
-                  style={{ backgroundColor: "#93A0B0" }}
-                  role="progressbar"
-                  aria-valuenow="75"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                ></div>
-              </div>
-              <small className="ml-2">45.00</small>
+            <div className="w-100 progressB d-flex align-items-baseline">
+              <small className="mr-2" style={{ color: "white" }}>
+                {this.time(this.state.currentSongTime)}
+              </small>
+              <input
+                // className="progress-bar w-75"
+                style={{ backgroundColor: "#93A0B0", width: "100%" }}
+                onChange={this.songDuration}
+                type="range"
+                name="progressBar"
+                id="prgbar2"
+                value={
+                  this.state.duration
+                    ? (this.state.currentSongTime * 100) / this.state.duration
+                    : 0
+                }
+                // aria-valuemin="0"
+                // aria-valuemax="100"
+              />
+
+              <small className="ml-2" style={{ color: "white" }}>
+                {this.time(this.state.duration)}
+              </small>
             </div>
             <audio
+              onTimeUpdate={(e) =>
+                this.setState({ currentSongTime: e.target.currentTime })
+              }
+              onCanPlay={(e) => this.setState({ duration: e.target.duration })}
               ref={this.volume}
               type="audio/mpeg"
               preload="true"
               autoPlay={true}
               src={this.props.playSong && this.props.playSong.preview}
             />
-            {/* <div className="nav-bottom d-flex justify-content-between w-100 d-lg-none d-md-none border-top border-dark pt-2 mt-2">
-                 <a href="albums.html" className="d-flex flex-column text-center"><i className="fa  fa-home " style={{fontSize : "1.5rem"}}></i>Home</a>
-                 <a href="#"className="d-flex flex-column text-center"><i className="fa  fa-search " style={{fontSize : "1.5rem"}}></i>Search</a>
-                 <a href="#" className="d-flex flex-column text-center"><i className="fa   fa-book  " style={{fontSize : "1.5rem"}}></i>Library</a>
-                 <a href="#" className="d-flex flex-column text-center"><i className="fab   fa-spotify" style={{fontSize : "1.5rem"}}></i>Premium</a>
-              </div> */}
           </Col>
 
           <Col className="d-flex justify-content-end align-items-center mt-2 ">
             {!this.state.mute ? (
               <a className="pr-3" onClick={() => this.muteVolume()}>
                 {" "}
-                <i
-                  style={{ color: "white" }}
-                  className=" fa fa-volume-mute"
-                ></i>
+                <i className=" fa fa-volume-up"></i>
               </a>
             ) : (
               <a className="pr-3" onClick={() => this.muteVolume()}>
-                <i className=" fa fa-volume-up"></i>
+                <i className=" fa fa-volume-off"></i>
               </a>
             )}
 
-            {/* <div style={{ height: "0.3rem" }}> */}
             <input
-              // className="progress-bar w-100"
               style={{ backgroundColor: "#93A0B0;" }}
               type="range"
               name="volBar"
-              aria-valuenow={Math.round(this.state.volumState * 100)}
-              aria-valuemin="0"
-              aria-valuemax="100"
+              value={Math.round(this.state.volumState * 100)}
+              // aria-valuemin="0"
+              // aria-valuemax="100"
               onChange={(e) => this.changeVolume(e.currentTarget.value / 100)}
-              // value={Math.round(this.state.volumState * 100)}
-              // type="range"
-              // name="volBar"
               id="volBar"
             />
-            {/* </div> */}
-            {/* <input
-              value={Math.round(this.state.volumState * 100)}
-              type="range"
-              name="volBar"
-              id="volBar"
-              // onChange={(e) => this.handleVolume(e.target.value / 100)}
-            /> */}
           </Col>
         </div>
       </div>
@@ -140,4 +182,4 @@ class Footer extends Component {
   }
 }
 
-export default connect(mapStateToProps)(Footer);
+export default connect(mapStateToProps, mapDispatchToProps)(Footer);
