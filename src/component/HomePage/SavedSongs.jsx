552ccf9726d7f24ea020ce1b0e130b1f529848ac
alekszone <50597224+alekszone@users.noreply.github.com>
2021-05-14 21:24:30 +0200

    like song

--- src/component/HomePage/SavedSongs.jsx
+++ src/component/HomePage/SavedSongs.jsx
@@ -1,14 +1,33 @@
 import React, { Component } from "react";
 import Styles from "../style.module.css";
 import { Row, Col, Card, Button } from "react-bootstrap";
+import { connect } from "react-redux";
+const mapStateToProps = (state) => state;
+const mapDispatchToProps = (dispatch, props) => ({
+  singelSong: (playSong) =>
+    dispatch({
+      type: "playSong",
+      payload: playSong,
+    }),
+});
 
-export default class Login extends Component {
+class SavedSongs extends Component {
+  convertTime = (time) => {
+    return (time - (time %= 60)) / 60 + (9 < time ? ":" : ":0") + time;
+  };
   render() {
+    console.log(this.props.likedSong, "fjejrgkekjrgkjhekjr");
     return (
       <div className={`${Styles.home}`}>
         <Row>
           <Col className="d-flex justify-content-center mt-5">
-            <Card style={{ width: "18rem", backgroundColor: "transparent" }}>
+            <Card
+              style={{
+                width: "18rem",
+                backgroundColor: "transparent",
+                height: "auto",
+              }}
+            >
               <Card.Img
                 variant="top"
                 src="https://w7.pngwing.com/pngs/867/694/png-transparent-user-profile-default-computer-icons-network-video-recorder-avatar-cartoon-maker-blue-text-logo.png"
@@ -22,10 +41,30 @@ export default class Login extends Component {
           </Col>
 
           <Col className="mt-5">
-            <h2 style={{ color: "white " }}>all liked songs</h2>
+            {this.props.likedSong &&
+              this.props.likedSong.map((song) => {
+                return (
+                  <>
+                    <div
+                      className="d-flex justify-content-around"
+                      style={{ color: "white" }}
+                    >
+                      <div className="d-flex flex-column">
+                        <h6>{song.artist.name}</h6>
+                        <h6>{song.title}</h6>
+                      </div>
+                      <div>
+                        <h6>{this.convertTime(song.duration)}</h6>
+                      </div>
+                    </div>
+                  </>
+                );
+              })}
+            <h2 style={{ color: "white " }}></h2>
           </Col>
         </Row>
       </div>
     );
   }
 }
+export default connect(mapStateToProps, mapDispatchToProps)(SavedSongs);
